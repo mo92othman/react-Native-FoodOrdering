c22c1b43ba0bcb06940561c1af1725145d85c804
mo92othman <mo92othman@gmail.com>
2024-07-25 17:16:08 +0200

    added a flatList component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,15 +1,26 @@
-import { StyleSheet, ScrollView } from 'react-native';
+import { StyleSheet, ScrollView, FlatList  } from 'react-native';
 import products from '@/assets/data/products';
 import ProductListItem from '@/src/components/ProductListItem';
 
 // MenuScreen Component
 const MenuScreen = () => {
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      {products.map((product, index) => (
-        <ProductListItem key={index} product={product} />
-      ))}
-    </ScrollView>
+    // <ScrollView contentContainerStyle={styles.container}>
+    //   {products.map((product, index) => (
+    //     <ProductListItem key={index} product={product} />
+    //   ))}
+    // </ScrollView>
+
+    <FlatList
+    data={products} 
+    renderItem={({ item }) => ( 
+      <ProductListItem key={item.id} product={item} /> 
+    )}
+    // keyExtractor={(item) => item.id}
+    numColumns={2}
+    contentContainerStyle={{gap: 10}}
+    columnWrapperStyle={{gap: 10}}
+  />
   );
 };
 
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -12,7 +12,7 @@ const defaultPizzaImage= 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/fo
 const ProductListItem = ({ product }:ProductListItemProps) => {
   return (
     <View style={styles.itemContainer}>
-      <Image style={styles.image} source={{ uri: product.image || defaultPizzaImage }} />
+      <Image style={styles.image} source={{ uri: product.image || defaultPizzaImage }} resizeMode='contain'/>
       <Text style={styles.productName}>{product.name}</Text>
       <Text style={styles.productPrice}>{product.price}</Text>
     </View>
@@ -25,18 +25,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginBottom: 20, 
+    flex: 1,
+    maxWidth: '50%',
   },
   image: {
-    width: '100%',
+    width: 150,
+    // height: 200,
     aspectRatio: 1,
     marginBottom: 10,
   },
   productName: {
     color: Colors.dark.tint,
-    fontSize: 40
+    fontSize: 18
   },
   productPrice: {
-    fontSize: 32,
+    fontSize: 24,
     color: Colors.light.tint,
     backgroundColor: 'yellow',
     marginTop: 20,
